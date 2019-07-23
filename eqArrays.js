2a603a18567354b1f7e2d5b3a89b7fb77a95b780
revSoul12 <daniel.laserna@gmail.com>
2019-07-23 22:23:45 +0000

    Made function more resusable

--- eqArrays.js
+++ eqArrays.js
@@ -7,14 +7,16 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
 
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
   } 
   return true;
-    
   };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
