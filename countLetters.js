6074a72752c079472c1c0142cae9ce8ff9d30cbc
revSoul12 <daniel.laserna@gmail.com>
2019-07-24 18:20:25 +0000

    Implemented countLetters assert

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔️⛔️⛔️ Assertion Failed:  ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  const results = {};
+  for (const item of str) {
+    if (item !== " ") {
+      if (results[item]) {
+        results[item]++;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+
+console.log(countLetters("lighthouse2 in the 411house"));
