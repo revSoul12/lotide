055383490b7b39c91530b6a02cd6a0fe48698a08
revSoul12 <daniel.laserna@gmail.com>
2019-07-24 00:25:41 +0000

    Created without filter

---
+++ without.js
@@ -0,0 +1,34 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } 
+  return true;
+  };
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔️⛔️⛔️ Assertion Failed:  ${actual} !== ${expected}`);
+  }
+};
+
+const without = function (source, itemsToRemove) {
+  let newArr = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])){
+    newArr.push(source[i]);
+    }
+  }
+  return newArr;
+};
+
+assertArraysEqual (without(["hello", "world", "lighthouse"], ["lighthouse"]), ["hello", "world"]);
+
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
