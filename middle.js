0949fd00d6c6160ebb75b7e06ec44ff966ffb1ba
revSoul12 <daniel.laserna@gmail.com>
2019-07-29 18:56:13 +0000

    Created middleTest.js

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  } 
-  return true;
-  };
-
-const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔️⛔️⛔️ Assertion Failed:  ${actual} !== ${expected}`);
-  }
-};
 
 const middle = function (arr) {
   
@@ -34,6 +14,6 @@ const middle = function (arr) {
   
 };
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 8]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+module.exports = middle;
+
+
